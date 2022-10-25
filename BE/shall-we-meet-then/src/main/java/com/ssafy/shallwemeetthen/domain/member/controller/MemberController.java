22ace238fc26db7a01226dc6783b3eb095ec64a9
DESKTOP-KVCQHCD\multicampus <>
2022-10-25 12:56:33 +0900

    [S07P22D102-29]  fix : Valid 예외 리턴 방식 수정

--- BE/shall-we-meet-then/src/main/java/com/ssafy/shallwemeetthen/domain/member/controller/MemberController.java
+++ BE/shall-we-meet-then/src/main/java/com/ssafy/shallwemeetthen/domain/member/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,15 +30,12 @@ public class MemberController {
     private final MemberAddService memberAddService;
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody @Validated MemberJoinRequestDto dto){
-       //try {
-
-
-       // System.out.println(errors.getFieldError().getDefaultMessage());
+        try {
             return new ResponseEntity<>(memberAddService.join(dto), HttpStatus.OK);
-//        } catch (IllegalStateException e) {
-//            System.out.println(e.getMessage());
-//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-//        }
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("/login")
