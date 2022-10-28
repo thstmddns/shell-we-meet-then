5cd1eb645afc6528794594d5fc75823122cbdf89
kunhee <alqp118@naver.com>
2022-10-28 16:58:47 +0900

    [S07P31D105-78]feat:메인 및 홈 화면 기본레이아웃 구성

--- shall-we-meet-then/src/pages/Main/Main.js
+++ shall-we-meet-then/src/pages/Main/Main.js
@@ -1,5 +1,5 @@
-import React, {useState,useEffect} from 'react'
-import { useNavigate } from  "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 import './Main.css'
 import { getGroupsApi } from '../../api/Main';
 
@@ -7,113 +7,118 @@ import { getGroupsApi } from '../../api/Main';
 function Main() {
 
   const test = [{
-		"groupSeq":1,
-		"name":'크리스마스',
-		"invitationCode":'fsdjk23fm',
-		"openDateTime":'2022-10-28 00:00:00',
-		"headcount":8,
-		"agree":true
-	},
+    "groupSeq": 1,
+    "name": '크리스마스',
+    "invitationCode": 'fsdjk23fm',
+    "openDateTime": '2022-10-28 00:00:00',
+    "headcount": 8,
+    "agree": true
+  },
   {
-		"groupSeq":2,
-		"name":'설날',
-		"invitationCode":'fsdjk23fm',
-		"openDateTime":'2022-10-20 00:00:00',
-		"headcount":8,
-		"agree":true
-	},]
+    "groupSeq": 2,
+    "name": '설날',
+    "invitationCode": 'fsdjk23fm',
+    "openDateTime": '2022-10-20 00:00:00',
+    "headcount": 8,
+    "agree": true
+  },]
 
-    const [groups, setGroups] = useState(test);
-    const [dDay, setDDay] = useState(null);
-    // 캐러셀용
-    const [temp, setTemp] = useState(0);
-    // 흐르는 시간 리스트
-    const [flowingList, setFlowingList] = useState([]);
-    // 흘러간 시간 리스트
-    const [flowedList, setFlowedList] = useState([]);
-
-
-    const now = new Date();
-    // const testTime = new Date(`${groups[temp].openDateTime} 00:00:00`);
-    const testTime = new Date(groups[temp].openDateTime);
+  const [groups, setGroups] = useState(test);
+  const [dDay, setDDay] = useState(null);
+  // 캐러셀용
+  const [temp, setTemp] = useState(0);
+  // 흐르는 시간 리스트
+  const [flowingList, setFlowingList] = useState([]);
+  // 흘러간 시간 리스트
+  const [flowedList, setFlowedList] = useState([]);
 
 
-    const checkButton = () => {
-      console.log(temp)
-    }
+  const now = new Date();
+  // const testTime = new Date(`${groups[temp].openDateTime} 00:00:00`);
+  const testTime = new Date(groups[temp].openDateTime);
 
 
-    useEffect(()=>{
-      setDDay(Math.ceil((now-testTime) / (1000 * 60 * 60 * 24)))
-    },[testTime])
+  const checkButton = () => {
+    console.log(temp)
+  }
 
-    useEffect(()=> {
-      const check1 = []
-      const check2 = []
-      // console.log(groups.length)
-      for(let i = 0; i < groups.length; i++){
-        const checkTime = new Date(groups[i].openDateTime);
-        const nowTime =  new Date();
-        
-        // 흐르는시간
-        if(Math.ceil((nowTime-checkTime) / (1000 * 60 * 60 * 24)) > 0){
-          check1.push(groups[i])
-        }
-        // 흘러간 시간
-        else{
-          check2.push(groups[i])
-        }
+
+  useEffect(() => {
+    setDDay(Math.ceil((now - testTime) / (1000 * 60 * 60 * 24)))
+  }, [testTime])
+
+  useEffect(() => {
+    const check1 = []
+    const check2 = []
+    // console.log(groups.length)
+    for (let i = 0; i < groups.length; i++) {
+      const checkTime = new Date(groups[i].openDateTime);
+      const nowTime = new Date();
+
+      // 흐르는시간
+      if (Math.ceil((nowTime - checkTime) / (1000 * 60 * 60 * 24)) > 0) {
+        check1.push(groups[i])
       }
-      setFlowingList(check1)
-      setFlowedList(check2)
-    },[])
-
-
-
-
-    // useEffect(()=>{
-    //   getGroupsApi().then(r => setGroups(r.data))
-    // },[])
-
-    const plusTemp = () => {
-      if (temp + 1 === groups.length){
-        return
+      // 흘러간 시간
+      else {
+        check2.push(groups[i])
       }
-      setTemp(temp+1);
     }
+    setFlowingList(check1)
+    setFlowedList(check2)
+  }, [])
 
-    const minusTemp = () => {
-      if (temp === 0 ){
-        return
-      }
-      setTemp(temp-1)
+
+
+
+  // useEffect(()=>{
+  //   getGroupsApi().then(r => setGroups(r.data))
+  // },[])
+
+  const plusTemp = () => {
+    if (temp + 1 === groups.length) {
+      return
     }
+    setTemp(temp + 1);
+  }
 
-
-    const navigate = useNavigate();
-    const goCreateGroup = () => {
-      navigate("/group/create");
+  const minusTemp = () => {
+    if (temp === 0) {
+      return
     }
-    const goWriteBoard = () => {
-      navigate(`/group/article/create/${groups[temp].groupSeq}`)
-    }
+    setTemp(temp - 1)
+  }
 
 
-    
+  const navigate = useNavigate();
+  const goCreateGroup = () => {
+    navigate("/group/create");
+  }
+  const goWriteBoard = () => {
+    navigate(`/group/article/create/${groups[temp].groupSeq}`)
+  }
+
+
+
 
 
   return (
     <div>
       <div>
-        <button onClick={checkButton}>테스트트트트</button>
-        <h1>D-day</h1>
-        <h1>D{dDay >= 0 ? '-' : '+'}
+        {/* <button onClick={checkButton}>테스트트트트</button> */}
+        {/* <h1>D-day</h1> */}
+
+        <button className='downBtn' onClick={minusTemp}> down </button>
+        <button className='upBtn' onClick={plusTemp}> up </button>
+
+        {/* <button onClick={goWriteBoard}>글 쓰러가기</button>
+        <button onClick={goCreateGroup}>그룹만들기 및 창여하기</button> */}
+        <div className='imgDiv'>
+          <h1 className='dDay'>D{dDay >= 0 ? '-' : '+'}
             {dDay === 0 ? 'day' : Math.abs(dDay)}</h1>
-        <button onClick={minusTemp}> down </button>
-        <button onClick={plusTemp}> up </button>
-        <br />
-        <button onClick={goWriteBoard}>글 쓰러가기</button>
-        <button onClick={goCreateGroup}>그룹만들기 및 창여하기</button>
+          <img className='watch' src={process.env.PUBLIC_URL + '/assets/img/watch.jpg'} />
+        </div>
+        <h1 className='groupName'>{groups[temp].name}</h1>
 
       </div>
 
