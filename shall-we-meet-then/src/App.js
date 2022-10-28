a317b8d2dbbfbac59d556bf0c5cd14b7546c8d65
kunhee <alqp118@naver.com>
2022-10-28 14:30:41 +0900

    [S07P31D105-131]feat:게시글 작성기능 완성

--- shall-we-meet-then/src/App.js
+++ shall-we-meet-then/src/App.js
@@ -11,7 +11,8 @@ import {
   MemoryList,
   Memory,
   Main,
-  Statistics
+  Statistics,
+  WriteBoard
 
 } from './pages/index'
 
@@ -32,6 +33,7 @@ function App() {
           <Route path='/group/memory/:groupSeq' element={<MemoryList />} />
           <Route path='/group/memory/:groupSeq/:articleSeq' element={<Memory />} />
           <Route path='/group/statistics/:groupSeq' element={<Statistics />} />
+          <Route path='/group/article/create/:groupSeq' element={<WriteBoard />} />
             
           
         </Routes>
