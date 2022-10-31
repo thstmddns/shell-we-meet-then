4194efb3006282a9a2749546a2f84ae09e6c5466
kunhee <alqp118@naver.com>
2022-10-31 17:43:38 +0900

    [S07P31D105-142]feat:메인페이지 및 그룹생성 그룹참여 ui/ux기본단계 끝

--- shall-we-meet-then/src/pages/CreateGroup/CreateGroup.js
+++ shall-we-meet-then/src/pages/CreateGroup/CreateGroup.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom';
 import { addGroup, addGroupMember } from '../../api/CreateGroupApi'
 import NavBar from "../../Components/NavBar/NavBar";
+import './CreateGroup.css'
 
+ 
 export default function CreateGroup() {
 
 
@@ -13,6 +16,11 @@ export default function CreateGroup() {
   // 그룹 참가용 state
   const [invitationCode, setInvitationCode] = useState('');
   const [joinNickname, setJoinNickname] = useState('');
+
+  //  useLocation 훅 취득
+  const location = useLocation();
+  const formTemp = location.state.temp
+
 
   // 그룹생성 함수
   const callAddGroup = () => {
@@ -67,22 +75,61 @@ export default function CreateGroup() {
 
 
 
+
+
+
+
+
   return (
     <div>
       <NavBar />
-      <input type='text' placeholder='그룹이름' value={groupName}
-        onChange={(e) => { setGroupName(e.target.value) }} />
-      <input type='text' placeholder='닉네임' value={nickName}
-        onChange={(e) => { setNickName(e.target.value) }} />
-      <input type='date' value={openDate}
-        onChange={(e) => { setOpenDate(e.target.value) }} />
-      <button onClick={callAddGroup}>그룹생성</button>
-      <br />
-      <input type='text' placeholder='그룹참여코드' value={invitationCode}
-        onChange={(e) => { setInvitationCode(e.target.value) }} />
-      <input type='text' placeholder='닉네임' value={joinNickname}
-        onChange={(e) => { setJoinNickname(e.target.value) }} />
-      <button onClick={joinGroup}>그룹참여하기</button>
+      <div className='imgDiv1'>
+        <img className='watch1' src={process.env.PUBLIC_URL + '/assets/img/watch.jpg'} />
+        <div className='inputs'>
+          {formTemp === 1 ?
+            <>
+              <h1>시계 만들기</h1>
+              <div>
+                <label htmlFor='groupName'>시계이름</label>
+                <input type='text' id='groupName' placeholder='그룹이름' value={groupName}
+                  onChange={(e) => { setGroupName(e.target.value) }} />
+              </div>
+              <br />
+              <div>
+                <label htmlFor='nickName'>닉네임</label>&nbsp;&nbsp;&nbsp;
+                <input type='text' id='nickName' placeholder='사용하실 닉네임을 입력해주세요' value={nickName}
+                  onChange={(e) => { setNickName(e.target.value) }} />
+              </div>
+              <br />
+              <div>
+                <label htmlFor='openDate'>방문 날짜</label>
+                <input type='date' id='openDate' value={openDate}
+                  onChange={(e) => { setOpenDate(e.target.value) }} />
+              </div>
+              <br />
+              <button onClick={callAddGroup}>그룹생성</button>
+            </>
+
+            : <>
+            <div>
+            <h1>시계에 참여하기</h1>
+              <input type='text' placeholder='그룹참여코드' value={invitationCode}
+                onChange={(e) => { setInvitationCode(e.target.value) }} />
+              <br />
+              <input type='text' placeholder='닉네임' value={joinNickname}
+                onChange={(e) => { setJoinNickname(e.target.value) }} />
+              <br />
+              <button onClick={joinGroup}>그룹참여하기</button>
+              </div></>}
+
+
+
+          <br />
+
+
+        </div>
+      </div>
+
     </div>
   )
 }
