f11b1b87913adbb2bf82b911a3daf00cde24cdc2
DESKTOP-KVCQHCD\multicampus <>
2022-11-01 13:45:05 +0900

    [S07P31D105-117]  feat : [S07P31D105-117]  feat : Get ModelAttribute

--- BE/shall-we-meet-then/src/main/java/com/ssafy/shallwemeetthen/common/security/filter/JwtAuthenticationFilter.java
+++ BE/shall-we-meet-then/src/main/java/com/ssafy/shallwemeetthen/common/security/filter/JwtAuthenticationFilter.java
@@ -6,11 +6,11 @@ import com.auth0.jwt.algorithms.Algorithm;
 import java.util.Date;
 
 public class JwtAuthenticationFilter {
-    String jwtToken = JWT.create()
-            .withSubject("INDIVE")
-            .withExpiresAt(new Date(System.currentTimeMillis()+1000*60*60*24*14))
-            .withClaim("seq",principalDetails.getMember().getSeq())
-            .withClaim("username", principalDetails.getMember().getEmail())
-            .sign(Algorithm.HMAC512(JwtProperties.SECRET));
+//    String jwtToken = JWT.create()
+//            .withSubject("INDIVE")
+//            .withExpiresAt(new Date(System.currentTimeMillis()+1000*60*60*24*14))
+//            .withClaim("seq",principalDetails.getMember().getSeq())
+//            .withClaim("username", principalDetails.getMember().getEmail())
+//            .sign(Algorithm.HMAC512(JwtProperties.SECRET));
 
 }
