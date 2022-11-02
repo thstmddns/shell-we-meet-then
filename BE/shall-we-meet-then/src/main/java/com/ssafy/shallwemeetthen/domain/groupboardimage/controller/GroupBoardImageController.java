d1328724fc000d1ffc44ea839e3b84e9b4933001
이명범 <mungmnb777@gmail.com>
2022-11-02 10:21:57 +0900

    [S07P31D105] feat: 그룹 보드 이미지 파일 조회 기능 추가

--- BE/shall-we-meet-then/src/main/java/com/ssafy/shallwemeetthen/domain/groupboardimage/controller/GroupBoardImageController.java
+++ BE/shall-we-meet-then/src/main/java/com/ssafy/shallwemeetthen/domain/groupboardimage/controller/GroupBoardImageController.java
@@ -1,4 +1,28 @@
 package com.ssafy.shallwemeetthen.domain.groupboardimage.controller;
 
+import com.ssafy.shallwemeetthen.domain.groupboard.service.GroupBoardService;
+import com.ssafy.shallwemeetthen.domain.groupboardimage.service.GroupBoardImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/board-image")
 public class GroupBoardImageController {
+
+    private final GroupBoardImageService groupBoardImageService;
+
+    @GetMapping(value = "/{boardImageSeq}/file-download", produces = "application/octet-stream")
+    public ResponseEntity<?> getImageFile(@PathVariable("boardImageSeq") Long boardImageSeq) {
+        try {
+            return new ResponseEntity<>(groupBoardImageService.getImageFile(boardImageSeq), HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
