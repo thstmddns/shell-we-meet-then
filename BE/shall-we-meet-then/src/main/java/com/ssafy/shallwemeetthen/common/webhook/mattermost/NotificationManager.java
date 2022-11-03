822ab1f6d3456f3483a4b83988938f4dd92eca31
이명범 <mungmnb777@gmail.com>
2022-11-03 16:08:33 +0900

    [S07P31D105] refactor: 테스트 코드 작성을 위한 설정 클래스 추가

--- BE/shall-we-meet-then/src/main/java/com/ssafy/shallwemeetthen/common/webhook/mattermost/NotificationManager.java
+++ BE/shall-we-meet-then/src/main/java/com/ssafy/shallwemeetthen/common/webhook/mattermost/NotificationManager.java
@@ -2,10 +2,8 @@ package com.ssafy.shallwemeetthen.common.webhook.mattermost;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
 @RequiredArgsConstructor
 public class NotificationManager {
 
