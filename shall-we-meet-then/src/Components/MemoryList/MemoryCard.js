f71c15c3f8cdee5f5b82a5473586a3501c2572c4
이동민 <jewellove1023@gmail.com>
2022-11-04 17:36:30 +0900

    [S07P31D105-103] feat: MemoryList페이지 Api연결 - 1

--- shall-we-meet-then/src/Components/MemoryList/MemoryCard.js
+++ shall-we-meet-then/src/Components/MemoryList/MemoryCard.js
@@ -4,12 +4,16 @@ import { getArticlesApi } from '../../api/MemoryApi.js'
 import '../../pages/MemoryList/MemoryList.css'
 import '../../Common.css';
 
-
 export default function MemoryCard(props) {
+  const baseURL = "http://k7d105.p.ssafy.io:8080"
+  useEffect(() => {
+    console.log(props.boardSeq);
+  },[])
   return (
     <>
     <div className='memory-article-img'>
       <div onClick={() => {props.setModalBtn(1)}} className='click-memory'>
+        {/* <img className='memory-img' alt="#" src={baseURL + `/boards/${props.boardSeq}/image-download`}/> */}
         <img className='memory-img' alt="#" src={process.env.PUBLIC_URL + '/assets/img/bp.jpg'}/>
       </div>
     </div>
--- shall-we-meet-then/src/api/MemoryApi.js
+++ shall-we-meet-then/src/api/MemoryApi.js
@@ -1,12 +1,20 @@
 import api from './api'
 
 // 글 리스트 조회
-const getArticlesApi = async() => {
-    return await api.get(`/boards`)
+const getArticlesApi = async(info) => {
+    return await api.get(`/boards`, info)
 }
 // 글 상세 조회
 const getArticleApi = async(boardSeq) => {
     return await api.get(`/boards/${boardSeq}`)
+}
+// 로그인 멤버 글 갯수 조회
+const getArticleCount = async(info) => {
+    return await api.get(`/boards/count`, info)
+}
+// 전체 맴버 글 갯수 조회
+const getTotalArticleCount = async(info) => {
+    return await api.get(`/boards/total-count`, info)
 }
 // 동영상 원본 파일 조회
 const getVideoApi = async(boardSeq) => {
@@ -20,11 +28,20 @@ const getThumbnailApi = async(boardSeq) => {
 const getImageApi = async(boardImageSeq) => {
     return await api.get(`/board-image/${boardImageSeq}/file-download`)
 }
+// 그룹 상세 조회
+const getGroupDetailsApi = async(groupSeq) => {
+    return await api.get(`/groups/${groupSeq}`)
+}
+
+
 
 export {
     getArticlesApi,
     getArticleApi,
     getVideoApi,
     getThumbnailApi,
-    getImageApi
+    getImageApi,
+    getArticleCount,
+    getTotalArticleCount,
+    getGroupDetailsApi,
   }
